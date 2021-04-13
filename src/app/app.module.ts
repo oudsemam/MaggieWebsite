2ca9da89ffae2e83cf933ff8d5603c12548477d8
Maggie Oudsema <maggieoudsema@gmail.com>
2021-04-13 19:06:47 -0400

    added major components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConstructionComponent } from './construction/construction.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TechstackComponent } from './techstack/techstack.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConstructionComponent
+    ConstructionComponent,
+    NavBarComponent,
+    BannerComponent,
+    AboutComponent,
+    ProjectsComponent,
+    TechstackComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/construction/construction.component.ts
+++ src/app/construction/construction.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-construction',
   templateUrl: './construction.component.html',
   styleUrls: ['./construction.component.css']
 })
 export class ConstructionComponent implements OnInit {
+
 
   constructor() { }
 
